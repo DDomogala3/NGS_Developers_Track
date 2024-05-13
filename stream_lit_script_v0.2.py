dadbf4a5577c002541c6dcf2544145a674e64a0e
DDomogala3 <ddd38@nau.edu>
2024-05-13 12:50:54 -0500

    added file downloading

--- stream_lit_script_v0.2.py
+++ stream_lit_script_v0.2.py
@@ -1,4 +1,30 @@
 import streamlit as st
+from pysam import bcftools
+import os
+path_file = os.path.dirname("/Users/danieldomogala/Documents/Personal/Codeacademy_notebooks/")
+st.write("This is the first streamlit app.")
+st.write("Please answer quiz question 1")
+st.user_info = "DDD"
+st.text_input("What is soft clipping an alignment file?: " )
+uploaded_file = st.file_uploader("Please upload BCF file: ")
+#uploaded_file = .join uploaded_file
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
 
-st.write("HAPPY MOTHER's DAY to my beautiful wife! !!!")
-st.balloons()
+    #region = '11:524657,5246555'
+    #Pyview = pysam.view(uploaded_file.name,region)
+    #print(Pyview)
+    #st.write(Pyview)
+
+    with open("Galaxy117-[HBB_Gene]_streamlit.vcf","w") as f:
+        HBB_vcf = bcftools.call(uploaded_file.name,"-o", "Galaxy117-[HBB_Gene].vcf", "-c")
+        st.write(HBB_vcf)
+        f.write(HBB_vcf)
+        
+st.download_button(
+    label="Download vcf",
+    data=vcf,
+    file_name="Galaxy117-[HBB_Gene]_streamlit.vcf",
+    mime="vcf",
