d43bb76013862a2ca5e88191fac00cdda5882e10
DDomogala3 <ddd38@nau.edu>
2024-05-12 22:46:34 -0500

    Update stream_lit_script_v0.1.py
    edited script for clairty

--- stream_lit_script_v0.1.py
+++ stream_lit_script_v0.1.py
@@ -9,6 +9,6 @@ if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
     st.write(bytes_data)
-region = '11:524657,5246555'
-Pyview = pysam.view(bytes_data,region)
-st.write(Pyview)
+    region = '11:524657,5246555'
+    Pyview = pysam.view(bytes_data,region)
+    st.write(Pyview)
