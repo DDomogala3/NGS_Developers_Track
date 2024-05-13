658f920351bd85030c5ad79605efaa7e3397093e
DDomogala3 <ddd38@nau.edu>
2024-05-12 21:47:30 -0500

    added more elements

---
+++ stream_lit_script_v0.3.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.write("HAPPY MOTHER's DAY to my beautiful wife NICOLE DOMOGALA !!!")
+st.balloons()
+message = st.chat_message("assistant")
+message.write("Hello Nikki")
