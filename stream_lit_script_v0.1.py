cc58da9122f3b12c730c70de712611936f5cfde6
DDomogala3 <ddd38@nau.edu>
2024-05-12 21:29:38 -0500

    added more streamlit functionality

---
+++ stream_lit_script_v0.1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.write("This is the first streamlit app.")
+st.write("Please answer quiz question 1")
+st.user_info = "DDD"
+st.text_input("What is soft clipping an alignment file?: " )
