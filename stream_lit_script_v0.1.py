d4a8f87c2093dcad6e26fab4d7cc6693a94427a7
DDomogala3 <ddd38@nau.edu>
2024-05-12 22:20:45 -0500

    added pysam function

--- stream_lit_script_v0.1.py
+++ stream_lit_script_v0.1.py
@@ -1,10 +1,10 @@
 import streamlit as st
-
+import pysam
 st.write("This is the first streamlit app.")
 st.write("Please answer quiz question 1")
 st.user_info = "DDD"
 st.text_input("What is soft clipping an alignment file?: " )
-data = st.file_uploader("Please upload BAM file: ")
+uploaded_file = st.file_uploader("Please upload BAM file: ")
 if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
