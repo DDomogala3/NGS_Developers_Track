5ea2eb6c0fd7ee2f64f130cf00f607571ef92afd
DDomogala3 <ddd38@nau.edu>
2024-05-12 22:08:23 -0500

    added file upload

--- stream_lit_script_v0.1.py
+++ stream_lit_script_v0.1.py
@@ -4,3 +4,8 @@ st.write("This is the first streamlit app.")
 st.write("Please answer quiz question 1")
 st.user_info = "DDD"
 st.text_input("What is soft clipping an alignment file?: " )
+data = st.uploader("Please upload BAM file: ")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
