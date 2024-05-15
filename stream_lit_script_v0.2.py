f2d87a2c2db6287abc16e8a25006ff402a1e9e59
DDomogala3 <ddd38@nau.edu>
2024-05-14 19:34:30 -0500

    input_format

--- stream_lit_script_v0.2.py
+++ stream_lit_script_v0.2.py
@@ -3,7 +3,7 @@ from pysam import bcftools
 import os
 from st_files_connection import FilesConnection
 conn = st.experimental_connection('local', type = FilesConnection)
-COSMIC = conn.read("Downloads/Galaxy73-[Cosmic_GenomeScreensMutant_v99_GRCh37.vcf.gz].vcf_bgzip", format=None,ttl=None)
+COSMIC = conn.read("Downloads/Galaxy73-[Cosmic_GenomeScreensMutant_v99_GRCh37.vcf.gz].vcf_bgzip",input_format='parquet',ttl=None)
 #conn = st.connection('gcs',type = FilesConnection)
 #COSMIC = conn.open("ngsappbucket/Galaxy73-[Cosmic_GenomeScreensMutant_v99_GRCh37.vcf.gz].vcf_bgzip",mode = "rb")
 #TEST_FS = conn.fs()
