0d5e086d4d56a0fa6c9ccc7a14e8ca981cede432
DDomogala3 <ddd38@nau.edu>
2024-05-15 20:03:41 -0500

    list_name

--- stream_lit_script_v0.2.py
+++ stream_lit_script_v0.2.py
@@ -77,7 +77,7 @@ if uploaded_file is not None:
   
     output = st.text_input("Please name your output vcf file: ")
     with fs.open("ngsappbucket/uploaded_file") as google_bam:
-        st.write(google_bam.name)
+        st.write(google_bam)
         generate_vcf(google_bam,output)
   #  cloud_pipeline(output_vcf)
 
