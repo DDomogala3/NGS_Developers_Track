327e244df248b7fdab2629169dc3bbc057459258
DDomogala3 <ddd38@nau.edu>
2024-05-16 12:44:33 -0500

    upload_attempt

--- GCFS_test_v0.py
+++ GCFS_test_v0.py
@@ -11,3 +11,11 @@ st.write(contents)
 with fs.open("ngsappbucket/uploaded_file", "rb") as bam_file:
   bam_bytes = fs.read_bytes(bam_file)
   st.write(bam_bytes)
+  
+uploaded_file = st.file_uploader("Please upload BCF file: ")
+if uploaded_file is not None:
+    fs.upload(uploaded_file,"ngsappbucket/uploaded_file", recursive=False, **kwargs)
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+#fs.upload(uploaded_file, , recursive=False, **kwargs)
